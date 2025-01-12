c5836ec04e689de9991989b4474a3f59fa9df867
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2025-01-12 21:52:29 +0330

    feat(home): add home

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Header />
   <main><router-view></router-view></main>
+  <Footer />
 </template>
