4bd628fab4851d3d052fcfd490fca209ba489d63
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2025-01-24 21:55:42 +0330

    feat(query): add staleTime of 1 min

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { VueQueryPlugin } from "@tanstack/vue-query";
+import {
+  VueQueryPlugin,
+  type VueQueryPluginOptions,
+} from "@tanstack/vue-query";
+
+const vueQueryPluginOptions: VueQueryPluginOptions = {
+  queryClientConfig: {
+    defaultOptions: { queries: { staleTime: 1 * 60 * 1000 } },
+  },
+};
 
 const app = createApp(App);
 app.use(router);
-app.use(VueQueryPlugin);
+app.use(VueQueryPlugin, vueQueryPluginOptions);
 app.mount("#app");
